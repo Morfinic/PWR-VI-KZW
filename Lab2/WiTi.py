d6486a39cca0f152f489e84fa31e83d7df4ffeca
morfinix <morfinic@proton.me>
2024-04-06 18:09:02 +0200

    Działa

--- Lab2/WiTi.py
+++ Lab2/WiTi.py
@@ -4,7 +4,11 @@ from timeit import timeit
 
 @timeit
 def WiTi(task):
-    n = len(task); N: int = 1 << n; F: list = [math.inf] * N; F[0] = 0
+    n = len(task)
+    N: int = 1 << n
+    F: list = [math.inf] * N
+    F[0] = 0
+    order = [[] for _ in range(N)]
 
     for procOrder in range(1, N):
         b = ('0' * (n - len(bin(procOrder)[2:])) + bin(procOrder)[2:])[::-1]
@@ -12,22 +16,11 @@ def WiTi(task):
 
         for idx, bit in enumerate(b):
             if int(bit):
-                F[procOrder] = min(F[procOrder], F[procOrder - 2 ** idx] + task[idx][1] * max(C - task[idx][2], 0))
+                koszt = F[procOrder - 2 ** idx] + task[idx][1] * max(C - task[idx][2], 0)
+                if F[procOrder] > koszt:
+                    F[procOrder] = koszt
+                    order[procOrder] = order[procOrder - 2 ** idx] + [idx]
 
-    b = ['1'] * n
-    order = [[-1, -1]] * n
-    autism = [-1] * n
-    for idx in range(n):
-        for i in range(n, 0, -1):
-            if b[n - i] == '0':
-                continue
-            tmp_b = b.copy()
-            tmp_b[n - i] = '0'
-            kara = F[int(''.join(tmp_b), 2)]
-            if kara > order[idx][1] and i not in autism:
-                order[idx] = [i, kara]
-                autism[idx] = i
-        b[n - autism[idx]] = '0'
+    order = [n - x for x in order[-1]]
 
-    print("Order:", autism[::-1])
-    return F[N - 1]
+    return F[N - 1], order
--- Lab2/main.py
+++ Lab2/main.py
@@ -7,19 +7,21 @@ def main():
     filePath: str = "./Dane/subDane/data."
 
     with open("dane_out.txt", 'w') as outFile:
-        for i in range(0 + 1):
+        for i in range(0, 11):
             dane: list[list[int]] = list()
 
             with open(filePath + str(10 + i) + ".txt", 'r') as f:
-                # print(f"Otwarto plik data.{10+i}.txt")
-
                 for line in f:
                     dane.append([
                         int(x)
                         for x in line.strip('\n').split(' ')
                     ])
 
-            outFile.write(f"Plik: data.{10+i}.txt\nOpt: {WiTi(dane)}\n\n")
+            opt, order = WiTi(dane[::-1])
+
+            outFile.write(f"Plik: data.{10+i}.txt\n"
+                          f"Opt: {opt}\n"
+                          f"Order: {order}\n\n")
 
 
 if __name__ == "__main__":
