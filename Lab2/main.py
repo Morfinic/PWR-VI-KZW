b351acde635daa56da86f33170290489165f3c4d
Morfinix <morfinic@proton.me>
2024-04-08 23:29:42 +0200

    Początek rozwiązywania NEH.

--- Lab2/main.py
+++ Lab2/main.py
@@ -1,28 +1,20 @@
 from WiTi import *
-from timeit import timeit
 
+filePath: str = "./Dane/subDane/data."
+out_f = open("dane_out.txt", 'w')
 
-@timeit
-def main():
-    filePath: str = "./Dane/subDane/data."
+for i in range(10 + 1):
+    dane: list[list[int]] = list()
 
-    with open("dane_out.txt", 'w') as outFile:
-        for i in range(0, 11):
-            dane: list[list[int]] = list()
+    with open(filePath + str(10 + i) + ".txt", 'r') as f:
+        # print(f"Otwarto plik data.{10+i}.txt")
 
-            with open(filePath + str(10 + i) + ".txt", 'r') as f:
-                for line in f:
-                    dane.append([
-                        int(x)
-                        for x in line.strip('\n').split(' ')
-                    ])
+        for line in f:
+            dane.append([
+                int(x)
+                for x in line.strip('\n').split(' ')
+            ])
 
-            opt, order = WiTi(dane[::-1])
+    out_f.write(f"Plik: data.{10+i}.txt\nOpt: {WiTi(dane)}\n\n")
 
-            outFile.write(f"Plik: data.{10+i}.txt\n"
-                          f"Opt: {opt}\n"
-                          f"Order: {order}\n\n")
-
-
-if __name__ == "__main__":
-    main()
+out_f.close()
---
+++ Lab3/main.py
@@ -0,0 +1,19 @@
+class Task:
+    name: str = ""
+    numOfTask: int = None
+    numOfMachines: int = None
+    NehAnswerCmax: int = None
+    # NehAnswerOrder = ""
+
+
+def main():
+    tmpList: list = list()
+    taskList: list = list()
+
+    with open("data/neh.data.txt", 'r') as f:
+
+
+
+
+if __name__ == "__main__":
+    main()
